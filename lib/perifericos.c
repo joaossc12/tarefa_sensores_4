25432b420ef1b531be7819604d09b5d1c471d588
João Vitor <joaossc.ege@gmail.com>
2025-08-19 13:33:11 -0300

    att

---
+++ lib/perifericos.c
@@ -0,0 +1,27 @@
+#include "perifericos.h"
+
+uint32_t rgb_u32(uint8_t r, uint8_t g, uint8_t b)
+{
+    return ((uint32_t)g << 24) | ((uint32_t)r << 16) | ((uint32_t)b << 8);
+}
+
+void alter_brightness(uint16_t lux, uint8_t *r, uint8_t *g, uint8_t *b)
+{
+    //Normaliza o valor do brilho para que fique entre 0 e 255
+    uint8_t brightness = lux > 1000 ? 255 : (lux * 255 / 1000);
+
+    //Ajusta as cores de acordo com o brilho obtido
+    (*r) = ((*r) * brightness) / 255;
+    (*g) = ((*g) * brightness) / 255;
+    (*b) = ((*b) * brightness) / 255;
+}
+
+void draw_on_matrix(PIO pio, uint sm, uint16_t lux, uint8_t r, uint8_t g, uint8_t b)
+{
+    alter_brightness(lux, &r, &g, &b);
+
+    for (int i = 0; i < NUM_PIXELS; i++)
+    {
+        pio_sm_put_blocking(pio, sm, rgb_u32(r, g, b));
+    }
+}
--- tarefa_sensores_4.c
+++ tarefa_sensores_4.c
@@ -5,11 +5,62 @@
 
 #include "lib/gy_33.h"
 #include "lib/bh1750_light_sensor.h"
+#include "lib/perifericos.h"
+#include "lib/ssd1306.h"
+#include "lib/buzzer.h"
 
+// Leds
+
+#define LED_RED       13
+#define LED_BLUE      12
+#define LED_GREEN     11
+
+// Limites e Offset para o Lux e alerta sonoro do buzzer
+#define MAX_LUX 1000
+#define MIN_LUX 10
+#define OFFSET_LUX 5
+
+//Variáveis globais para utilização da matriz de leds
+PIO pio = pio0;
+uint offset;
+uint sm;
+
+// Varável para alternar LEDS
+
+uint flag_leds = 0;
+
+ssd1306_t ssd;
+
+// Trecho para modo BOOTSEL com botão B
+#include "pico/bootrom.h"
+#define botaoB 6
+#define BUTTON_A      5
+void gpio_irq_handler(uint gpio, uint32_t events); //Função de callback dos botões
+void Control_Leds(uint flag); //Função de controle dos Leds
 
 int main()
 {
     stdio_init_all();
+
+    //Inicia Leds
+    gpio_init(LED_BLUE);
+    gpio_init(LED_GREEN);
+    gpio_init(LED_RED);
+    gpio_set_dir(LED_BLUE, GPIO_OUT);
+    gpio_set_dir(LED_RED, GPIO_OUT);
+    gpio_set_dir(LED_GREEN, GPIO_OUT);
+
+     // Para ser utilizado o modo BOOTSEL com botão B
+    gpio_init(botaoB);
+    gpio_set_dir(botaoB, GPIO_IN);
+    gpio_pull_up(botaoB);
+    gpio_set_irq_enabled_with_callback(botaoB, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+
+    gpio_init(BUTTON_A);
+    gpio_set_dir(BUTTON_A, GPIO_IN);
+    gpio_pull_up(BUTTON_A);
+    gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    // Fim do trecho para modo BOOTSEL com botão B
 
     // Inicializa o I2C0
     i2c_init(I2C_PORT, 400 * 1000);
@@ -42,11 +93,75 @@ int main()
         gy33_read_color(&r, &g, &b, &c);
         printf("Cor detectada - R: %d, G: %d, B: %d, Clear: %d\n", r, g, b, c);
 
-        sprintf(str_red, "%d R", r);   // Converte o inteiro em string
-        sprintf(str_green, "%d G", g); 
-        sprintf(str_blue, "%d B", b);  
-        sprintf(str_clear, "%d C", c);
+        sprintf(str_red, "%d", r);   // Converte o inteiro em string
+        sprintf(str_green, "%d", g); 
+        sprintf(str_blue, "%d", b);  
+        sprintf(str_clear, "%d", c);
+
+         Control_Leds(flag_leds);
+
+        //Aciona a matriz de acordo com os valores lidos
+        draw_on_matrix(pio, sm, lux, r, g, b);
+
+        // Desenha os valores no display OLED
+        draw_display(&ssd, str_red, str_green, str_blue, str_clear, str_lux);
+
+        // Liga/Desliga alerta sonoro de limites do Lux
+        handle_buzzer_warning(lux, MIN_LUX, MAX_LUX, OFFSET_LUX);
 
         sleep_ms(500);
     }
+}
+
+//Inicializa a matriz de LEDS
+void init_matrix()
+{
+    offset = pio_add_program(pio, &pio_matrix_program);
+    sm = pio_claim_unused_sm(pio, true);
+    pio_matrix_program_init(pio, sm, offset, MATRIX);
+}
+void Control_Leds(uint flag){
+    switch (flag)
+    {
+    case 1:
+        gpio_put(LED_BLUE, 0);
+        gpio_put(LED_GREEN, 0);
+        gpio_put(LED_RED, 1);
+        break;
+    case 2:
+        gpio_put(LED_BLUE, 1);
+        gpio_put(LED_GREEN, 0);
+        gpio_put(LED_RED, 0);
+        break;
+    case 3:
+        gpio_put(LED_BLUE, 0);
+        gpio_put(LED_GREEN, 1);
+        gpio_put(LED_RED, 0);
+        break; 
+
+
+    default:
+        gpio_put(LED_BLUE, 0);
+        gpio_put(LED_GREEN, 0);
+        gpio_put(LED_RED, 0);
+        break;
+    }
+}
+void gpio_irq_handler(uint gpio, uint32_t events)
+{
+    static absolute_time_t last_time_A = 0;
+    static absolute_time_t last_time_B = 0;
+    absolute_time_t now = get_absolute_time();
+
+    if (gpio == BUTTON_A) {
+        if (absolute_time_diff_us(last_time_A, now) > 200000) { // 200ms debounce
+            flag_leds = (flag_leds + 1)%4;
+            }
+            last_time_A = now;}
+
+    if (gpio == botaoB) {
+        if (absolute_time_diff_us(last_time_A, now) > 200000) { // 200ms debounce
+            reset_usb_boot(0, 0);
+            }
+            last_time_B = now;}
 }
